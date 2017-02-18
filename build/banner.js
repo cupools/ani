075afa71cc56acf610280205efb2ce7fdc79a7b0
liyh <lyh106415@alibaba-inc.com>
2017-02-18 23:40:02 +0800

    build: rollup to bundle browser module

--- build/banner.js
+++
@@ -1,6 +0,0 @@
-const pkg = require('../package.json')
-
-module.exports = [
-  `v${pkg.version} ${new Date().toLocaleDateString()}`,
-  `${pkg.homepage}`
-].join('\n')
--- build/webpack.base.js
+++ build/webpack.base.js
@@ -2,10 +2,11 @@ const path = require('path')
 
 module.exports = {
   entry: {
-    'velocity-component': './velocity-component/velocity-component.js'
+    'velocity-component': './velocity-component/src/velocity-component.js'
   },
   output: {
     filename: '[name].js',
+    sourceMapFilename: '[file].map',
     path: path.resolve('lib/'),
     library: '[name]',
     libraryTarget: 'umd',
